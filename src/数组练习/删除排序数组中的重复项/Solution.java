70c510cbc88275f7e996610f0e2ac2db6137dcf0
hechenbo <1377115660@qq.com>
2021-01-06 23:21:20 +0800

    数组+双指针

---
+++ src/数组练习/删除排序数组中的重复项/Solution.java
@@ -0,0 +1,11 @@
+package 数组练习.删除排序数组中的重复项;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j])nums[++i] = nums[j];
+        }
+        return i+1;
+    }
+}
