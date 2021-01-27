8a3146d1159e15e85dda7e7ae855577ad26e8b55
hechenbo <1377115660@qq.com>
2021-01-27 15:01:42 +0800

    寻找中心数组1.27

--- src/LeetCode/寻找数组的中心索引/Solution.java
+++ src/LeetCode/寻找数组的中心索引/Solution.java
@@ -27,7 +27,6 @@ public class Solution {
     public int pivotIndex(int[] nums) {
         int sum = 0;
         for (int num : nums) {
-            sum += num;
         }
         int leftSum = 0;
         for (int i = 0; i < nums.length; i++) {
