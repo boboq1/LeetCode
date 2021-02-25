889312c7cc4e59b52675c41f5abf8505f934b82a
hechenbo <1377115660@qq.com>
2021-02-25 08:19:29 +0800

    完全平方数-2.24

--- src/LeetCode/完全平方数1/Solution.java
+++ src/LeetCode/完全平方数1/Solution.java
@@ -1,6 +1,6 @@
 package LeetCode.完全平方数1;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @Author: hcb
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * @Version 1.0
  */
 public class Solution {
-        public int numSquares(int n) {
+       /* public int numSquares(int n) {
             int[] dp = new int[n + 1];
             Arrays.fill(dp,Integer.MAX_VALUE);
             dp[0] = 0;
@@ -24,9 +24,34 @@ public class Solution {
                 }
             }
             return dp[n];
-        }
+        }*/
+       public int numSquares(int n) {
+           List<Integer> squareArrays = new LinkedList<>();
+           for (int i = 1; i * i <= n; i++) {
+               squareArrays.add(i * i);
+           }
+           Set<Integer> queue = new HashSet<>();
+           queue.add(n);
+           int high = 0;
+           while (queue.size() != 0) {
+               high += 1;
+               Set<Integer> set = new HashSet<>();
+               for (Integer reminder : queue) {
+                   for (Integer squareArray : squareArrays) {
+                       if (reminder.equals(squareArray)) {
+                           return high;
+                       } else if (reminder < squareArray) break;
+                       else {
+                           set.add(reminder - squareArray);
+                       }
+                   }
+               }
+               queue = set;
+           }
+           return high;
+       }
 
-        public static void main(String[] args) {
+      /*  public static void main(String[] args) {
             new Solution().numSquares(12);
-        }
+        }*/
 }
